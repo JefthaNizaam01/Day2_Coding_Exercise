d0547db9f3160ecd37ac562529e3e4cce1cbc578
Name <nizaam01.jeftha@gmail.com>
2024-05-14 14:05:56 +0200

    initial commit

---
+++ script/code.js
@@ -0,0 +1,13 @@
+let firstName = "Nizaam";
+let lastName = "Jeftha";
+
+console.log(firstName + " " + lastName)
+
+let numberVar = 5;
+
+console.log(numberVar*5);
+
+const firstName01 = "Nizaamo";
+firstName01 = "Nightowl";
+
+//Yes I got an error.
